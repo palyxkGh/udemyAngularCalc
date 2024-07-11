0bb164880743481bc7df8228a8b6e3d41fc080ef
p.kosc <p.kosc@resolution.de>
2024-07-11 10:22:34 +0200

    calc with service upgrade & modules

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import {bootstrapApplication, platformBrowser} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppModule} from '../app.module';
 
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
